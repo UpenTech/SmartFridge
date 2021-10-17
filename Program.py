972358057a7c20146af5dad6ea120dfcef33583f
ndeu <gautamupen99@gmail.com>
2021-10-17 15:52:46 +0545

    Shopping Cart Added

--- Program.py
+++ Program.py
@@ -24,13 +24,13 @@ while valid != '0':
 		print(Fore.YELLOW + f"Listing Ingridents for {select}")
 		print(Fore.YELLOW + "-----------------------------------------------------")
 
-		for item in recipes[select]:	#Listing the key Ingredients
+		for item in recipes[select]:	#Listing the key Ingredients and checking its Availablity
 			print(Fore.YELLOW + item, end=":")
 			if item in pantry:
 				print(Fore.GREEN + "OK")
 			else:
 				print(Fore.RED + "OUT OF STOCK!!")
-				if not item in itemsBuy:
+				if not item in itemsBuy:	#Add to Shopping Cart
 					itemsBuy.append(item)
 
 if itemsBuy == []:
